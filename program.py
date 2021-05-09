0f0ab27c30a3ffe15ce49d55f64f3899fb2018dd
fitg <gustaw.fit@gmail.com>
2021-05-09 21:25:43 +0100

    Changed the main loop to use iterators

--- program.py
+++ program.py
@@ -1,5 +1,5 @@
 import argparse
-from typing import Any, List, Tuple
+from typing import Any, Generator, List, Tuple
 
 MIN_VALUE = 1
 MAX_VALUE = 1000000
@@ -7,21 +7,13 @@ DIVISOR = 2
 
 
 def find_all_ancestors(node1: int, node2: int) -> Tuple[List[int], List[int]]:
+    return list(ancestors(node1)), list(ancestors(node2))
 
-    x: int = node1
-    y: int = node2
-    ancestors_node1: List[int] = []
-    ancestors_node2: List[int] = []
 
-    while x >= MIN_VALUE:
-        x //= DIVISOR
-        ancestors_node1.append(x)
-
-    while y >= MIN_VALUE:
-        y //=DIVISOR
-        ancestors_node2.append(y)
-
-    return ancestors_node1, ancestors_node2
+def ancestors(node: int) -> Generator[int, None, None]:
+    while node >= MIN_VALUE:
+        node //= DIVISOR
+        yield node
 
 
 def find_lowest_common_ancestor(ancestors1: List[int], ancestors2: List[int]) -> int:
@@ -32,20 +24,26 @@ def find_lowest_common_ancestor(ancestors1: List[int], ancestors2: List[int]) ->
     # sort the list in descending order to find the ancestor
     common.sort(reverse=True)
     # return the top element as the lowest common ancestor
-    return int(common[0]) if len(common) else 1  # type: ignore
+    return int(common[0]) if len(common) else MIN_VALUE  # type: ignore
 
 
 def input_value(input_value: str) -> int:
     result = int(input_value)
     if result not in range(MIN_VALUE, MAX_VALUE):
-        raise argparse.ArgumentTypeError(f"input must be a valid integer between {MIN_VALUE} and {MAX_VALUE}, instead got {input_value}")
+        raise argparse.ArgumentTypeError(
+            f"input must be a valid integer between {MIN_VALUE} and {MAX_VALUE}, instead got {input_value}"
+        )
     return result
 
 
 def user_input() -> Any:
     parser = argparse.ArgumentParser(description="Find the lowest common ancestor for two binary tree nodes")
     parser.add_argument(
-        "integers", metavar="node id", type=input_value, nargs=2, help=f"two integer node ids; value {MIN_VALUE} >= N <={MAX_VALUE}"
+        "integers",
+        metavar="node id",
+        type=input_value,
+        nargs=2,
+        help=f"two integer node ids; value {MIN_VALUE} >= N <={MAX_VALUE}",
     )
 
     args = parser.parse_args()
